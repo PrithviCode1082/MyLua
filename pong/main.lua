0dadc21226830c3f493b3d556ac577024f646bdd
Prithvi <prithvikulahalli@gmail.com>
2025-10-03 14:31:25 +0530

    Menu screen in progress!

--- pong/main.lua
+++ pong/main.lua
@@ -1,14 +1,29 @@
-local Game = require('game')
+-- local Game = require("game")
 
 function love.load()
-    Game.load()
+    -- Images
+    menuBG = love.graphics.newImage('Images/menuImage/menuForest.png');
+    sunBG = love.graphics.newImage('Images/menuImage/sun.png')
+    panelBG1 = love.graphics.newImage('Images/menuImage/panel.png')
+    panelBG2 = love.graphics.newImage('Images/menuImage/panel.png')
+    panelBG3 = love.graphics.newImage('Images/menuImage/panel.png')
+    exit = love.graphics.newImage('Images/menuImage/exit.png')
+
+    -- Font
+    customFont = love.graphics.newFont("myfont.ttf", 24)
 end
 
 function love.update(dt)
-    Game.update(dt)
+
 end
 
 function love.draw()
-    Game.draw()
-end
+    love.graphics.draw(menuBG, 0, 0, 0, 0.8, 0.7)
+    love.graphics.draw(sunBG, 40, 20, 0, 1, 1)
+    love.graphics.draw(panelBG1, 320, 120, 0, 2, 1.5)
+    love.graphics.draw(panelBG2, 570, 50, 0, 2, 1.5)
+    love.graphics.draw(panelBG3, 570, 220, 0, 2, 1.5)
+    love.graphics.draw(exit, 30, 470, 0, 2, 1.5)
 
+    love.graphics.print("PONG", 500, 470, 0, 4, 4)
+end
